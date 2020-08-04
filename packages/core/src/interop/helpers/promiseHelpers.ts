3726ee9ecf1a6e2cafec522f6e2f929f77d51660
Kiril Popov <kkpopov@tick42.com>
2020-08-04 15:28:21 +0300

    core: cancel reject timeout when promise is resolved

--- packages/core/src/interop/helpers/promiseHelpers.ts
+++ packages/core/src/interop/helpers/promiseHelpers.ts
@@ -3,10 +3,18 @@ export const waitFor = async (ms: number = 0, callback: () => void) => {
     callback();
 };
 
-export function resolveAfter<T>(ms: number = 0, result?: T): Promise<T> {
+function resolveAfter<T>(ms: number = 0, result?: T): Promise<T> {
     return new Promise((resolve) => setTimeout(() => resolve(result), ms));
 }
 
-export function rejectAfter<T>(ms: number = 0, error?: T): Promise<T> {
-    return new Promise((resolve, reject) => setTimeout(() => reject(error), ms));
+export function rejectAfter<T>(ms: number = 0, promise: Promise<T>, error?: T): Promise<T> {
+    let timeout: any;
+    promise.finally(() => {
+        if (timeout) {
+            clearTimeout(timeout);
+        }
+    });
+    return new Promise((resolve, reject) => {
+        timeout = setTimeout(() => reject(error), ms);
+    });
 }
