4078f4bec89aa1d79b32db807e241516bd489012
Kiril Popov <kkpopov@hotmail.com>
2020-11-02 10:19:06 +0200

    core - fix application property in internal config (was failing in NodeJS)

--- packages/core/src/config.ts
+++ packages/core/src/config.ts
@@ -177,11 +177,20 @@ export default function (configuration: Glue42Core.Config, ext: Glue42Core.Exten
     }
 
     const connection = getConnection();
-    const windowAsAny = window as any;
+    let application: string = getApplication();
+    if (typeof window !== "undefined") {
+        const windowAsAny = window as any;
+        const containerApplication = windowAsAny.htmlContainer ?
+            `${windowAsAny.htmlContainer.containerName}.${windowAsAny.htmlContainer.application}` :
+            windowAsAny?.glue42gd?.application;
+        if (containerApplication) {
+            application = containerApplication;
+        }
+    }
 
     return {
         bus: configuration.bus ?? false,
-        application: windowAsAny.htmlContainer ? `${windowAsAny.htmlContainer.containerName}.${windowAsAny.htmlContainer.application}` : windowAsAny?.glue42gd?.application,
+        application,
         auth: getAuth(),
         logger: getLogger(),
         connection,
