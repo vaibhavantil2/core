64c21b52eade0c16616863abd40d4bda1ac52506
Kalin Kostov <k.kostov.eu@gmail.com>
2021-05-17 17:30:41 +0300

    fix: removed parentModule check which lead to compilation errors in some versions of angular
    Signed-off-by: Kalin Kostov <k.kostov.eu@gmail.com>

--- packages/ng/changelog.md
+++ packages/ng/changelog.md
@@ -1,3 +1,5 @@
+2.1.10
+fix: removed parentModule check which lead to compilation errors in some versions of angular
 2.1.9
 chore: bump due to dependencies update
 2.1.8
--- packages/ng/src/lib/ng.module.ts
+++ packages/ng/src/lib/ng.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, APP_INITIALIZER, ModuleWithProviders, Optional, SkipSelf } from "@angular/core";
+import { NgModule, APP_INITIALIZER, ModuleWithProviders } from "@angular/core";
 import { Glue42Store } from "./glue-store.service";
 import { Glue42NgSettings } from "./types";
 import { Glue42Initializer } from "./glue-initializer.service";
@@ -10,13 +10,6 @@ export const initFactory = (initializer: Glue42Initializer) => (): void | Promis
 // @dynamic
 @NgModule()
 export class Glue42Ng {
-
-    constructor(@Optional() @SkipSelf() parentModule?: Glue42Ng) {
-        if (parentModule) {
-            throw new Error("Glue42Ng Module is already loaded. Import it in the AppModule only");
-        }
-    }
-
     public static forRoot(settings?: Glue42NgSettings): ModuleWithProviders<Glue42Ng> {
         return {
             ngModule: Glue42Ng,
